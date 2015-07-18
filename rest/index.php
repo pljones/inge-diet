dfca115db0948e39c0e28f58221051b31e17d2f9
Peter L Jones <peter@drealm.info>
2015-07-19 00:32:18 +0100

    Admit some responsibility for the mess I am making

--- rest/index.php
+++ rest/index.php
@@ -9,6 +9,8 @@ $clients = [];
 *
 * ArrestDB 1.9.0 (github.com/alixaxel/ArrestDB/)
 * Copyright (c) 2014 Alix Axel <alix.axel@gmail.com>
+*
+* Hackery (C) 2015 Peter L Jones <pljones@users.sf.net>
 **/
 
 if (strcmp(PHP_SAPI, 'cli') === 0)
